f0316a0f0d11089dca4e625b801b30f310b9a1d5
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-09-26 00:18:07 +0600

    Update 270A - Fancy Fence.cc

--- 270A - Fancy Fence.cc
+++ 270A - Fancy Fence.cc
@@ -7,7 +7,7 @@ int main() {
     while (t--) {
         int x;
         cin >> x ;
-        if(360%(180-x)==0){
+        if(360%(180-x)==0){// i am using this because it is the formula 
           cout<<"YES"<<endl;
         }
         else{
