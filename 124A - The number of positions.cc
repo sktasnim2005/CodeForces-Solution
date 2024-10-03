f802f60211d78ce1182366c24b76e6aaca2c40cb
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-03 17:04:56 +0600

    Create 124A - The number of positions.cc

---
+++ 124A - The number of positions.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    if (n-a > b+1) {
+        cout << b+1 << endl;
+    }
+    else {
+        cout << n-a << endl;
+    }
+
+    return 0;
+}
