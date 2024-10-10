f3e45843ee0c6383588f60542367e26218670352
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-11 03:13:14 +0600

    Update 144A - Arrival of the General.cc

--- 144A - Arrival of the General.cc
+++ 144A - Arrival of the General.cc
@@ -5,21 +5,21 @@ int main() {
     int n;
     cin >> n;
     vector<int> a(n);
-    int max_v = 0, min_v = 101, max_i, min_i;
+    int max_v = 0, min_v = 101, max_i, min_i; // min_v = 101 because its highest value is 100 see problemset 
     for (int i = 0; i < n; i++) {
         cin >> a[i];
         if (a[i] > max_v) {
             max_v = a[i];
             max_i = i;
         }
-        if (a[i] <= min_v) {
+        if (a[i] <= min_v) { // there may be many same smallest value
             min_v = a[i];
             min_i = i;
         }
     }
 
-    int moves = max_i + (n - 1 - min_i);
-    if (max_i > min_i) {
+    int moves = max_i + (n - 1) - min_i;
+    if (max_i > min_i) {//If the tallest soldier is before the shortest soldier, reduce the move count by one to avoid double counting.
         moves--; // Adjust for overlap
     }
 
