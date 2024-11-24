7747d6fbf75029080f5e497f573010bce1b912e6
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-11-24 09:16:30 +0600

    Create 2039A - Shohag Loves Mod.cc

---
+++ 2039A - Shohag Loves Mod.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            a[i]=1+ i*2;
+            cout<<a[i]<<" ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
