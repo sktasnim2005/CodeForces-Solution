abc1ddaff5b7a148b613f7d157e48d2d585c325b
sktasnim2005 <sktasnim2005@gmail.com>
2025-05-06 02:33:04 +0600

    message

---
+++ A_Cloudberry_Jam.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    cout << n*2 << endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+	int t;
+	//int t=1;
+	cin>>t;
+	while (t--) {
+		solve();
+	}
+
+    return 0;
+}
---
+++ A_Ideal_Generator.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int k;
+    cin >> k;
+
+    cout << (k%2==0 ? "NO":"YES") << endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+	int t;
+	//int t=1;
+	cin>>t;
+	while (t--) {
+		solve();
+	}
+
+    return 0;
+}
